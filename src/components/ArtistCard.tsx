0cb1465f17813e7a0ea3ef8aafb7a63d0d98db92
Nicolò Camporese <62802984+camporesen@users.noreply.github.com>
2026-04-03 11:36:01 +0000

    mancava il tag

--- src/components/ArtistCard.tsx
+++ src/components/ArtistCard.tsx
@@ -205,7 +205,7 @@ export default function ArtistCard({ artist, ratings, members, userId, config, o
         )}
         {/* Link Spotify */}
         
-          href={spotify.url}
+        <a href={spotify.url}
           target="_blank"
           rel="noopener noreferrer"
           onClick={e => e.stopPropagation()}
