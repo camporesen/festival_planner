c9a9e17097593b7621fa0e2106f204eb0546c415
Nicolò Camporese <62802984+camporesen@users.noreply.github.com>
2026-04-04 13:59:52 +0000

    navtest

--- src/components/BottomNavWrapper.tsx
+++ src/components/BottomNavWrapper.tsx
@@ -1,12 +1,6 @@
 'use client'
-import { usePathname } from 'next/navigation'
 import BottomNav from './BottomNav'
 
-const HIDDEN_PATHS = ['/', '/login', '/forgot-password', '/reset-password', '/join']
-
 export default function BottomNavWrapper() {
-  const pathname = usePathname()
-  const hide = HIDDEN_PATHS.some(p => pathname.startsWith(p))
-  if (hide) return null
   return <BottomNav />
 }
