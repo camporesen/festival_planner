9fdb26d0a80c4c909afa1da060f1f71bdc7fb1d0
Nicolò Camporese <62802984+camporesen@users.noreply.github.com>
2026-04-04 13:34:09 +0000

    fix cache

--- next.config.ts
+++ next.config.ts
@@ -1,59 +1,7 @@
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
-  runtimeCaching: [
-    {
-      urlPattern: /^https:\/\/iexjechrqbhqsxlxxcgv\.supabase\.co\/rest\/.*/i,
-      handler: 'NetworkFirst',
-      options: {
-        cacheName: 'supabase-api',
-        expiration: {
-          maxEntries: 200,
-          maxAgeSeconds: 24 * 60 * 60,
-        },
-        networkTimeoutSeconds: 5,
-      },
-    },
-    {
-      urlPattern: /^https:\/\/iexjechrqbhqsxlxxcgv\.supabase\.co\/auth\/.*/i,
-      handler: 'NetworkOnly',
-    },
-    {
-      urlPattern: /\/_next\/static\/.*/i,
-      handler: 'CacheFirst',
-      options: {
-        cacheName: 'static-assets',
-        expiration: {
-          maxEntries: 200,
-          maxAgeSeconds: 30 * 24 * 60 * 60,
-        },
-      },
-    },
-    {
-      urlPattern: /\/_next\/image\?.*/i,
-      handler: 'CacheFirst',
-      options: {
-        cacheName: 'images',
-        expiration: {
-          maxEntries: 100,
-          maxAgeSeconds: 7 * 24 * 60 * 60,
-        },
-      },
-    },
-    {
-      urlPattern: /^https:\/\/i\.scdn\.co\/.*/i,
-      handler: 'CacheFirst',
-      options: {
-        cacheName: 'spotify-images',
-        expiration: {
-          maxEntries: 200,
-          maxAgeSeconds: 7 * 24 * 60 * 60,
-        },
-      },
-    },
-  ],
-})
+import type { NextConfig } from "next";
 
-module.exports = withPWA({})
+const nextConfig: NextConfig = {
+  turbopack: {}
+};
+
+export default nextConfig;
